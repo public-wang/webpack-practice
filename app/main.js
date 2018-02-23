16b96e0ba20335c062a5f2621dc35764d1e93ecc
public <631940441@qq.com>
2018-02-23 19:36:35 +0800

    add first

---
+++ app/main.js
@@ -0,0 +1,9 @@
+//main.js
+// const greeter = require('./Greeter');
+// document.querySelector("#root").appendChild(greeter());
+
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports= {
+    devtool: 'eval-source-map',
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
